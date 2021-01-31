dbc265570b72c2bbd6cc9140a82ac9a33a90d94e
Mor Bargig <Morbargig@gmail.com>
2021-01-31 12:52:15 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,7 @@
 export const environment = {
   production: true,
-  adminPassword: "bargig123456",
+  adminPassword: "???",
   firebaseConfig: {
-    apiKey: "AIzaSyB1vBJ_5kg4ZsbQZYd5TO1dxhvNw3Good0",
-    authDomain: "morbargig-a81d2.firebaseapp.com",
-    databaseURL: "https://morbargig-a81d2.firebaseio.com",
-    projectId: "morbargig-a81d2",
-    storageBucket: "gs://morbargig-a81d2.appspot.com",
-    messagingSenderId: "***",
-    appId: "1:475426301650:web:eead04a34d707791",
-    measurementId: "475426301650",
+    ???
   }
 };
